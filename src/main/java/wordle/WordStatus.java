1f75c5428b3b02ae1705a18f2b40687a69d295a4
Okies <ja00800@nate.com>
2022-07-24 16:35:59 +0900

    feat : 숫자 3개 힌트 판단 로직 구현
    3자리 수 Word STRIKE, BALL, NOTHING 개수 판단 로직 구현

--- src/main/java/wordle/WordStatus.java
+++ src/main/java/wordle/WordStatus.java
@@ -2,5 +2,17 @@ package wordle;
 
 public enum WordStatus {
 
-    STRIKE, BALL, NOTHING
+    STRIKE, BALL, NOTHING;
+
+    public boolean isNotMatching() {
+        return this != NOTHING;
+    }
+
+    public boolean isStrike() {
+        return this == STRIKE;
+    }
+
+    public boolean isBall() {
+        return this == BALL;
+    }
 }
